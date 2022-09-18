a4b75fc8ecff24cc7ccc5ca6367cda73b68a7256
Света <svetiksoleil@gmail.com>
2022-09-19 00:08:28 +0300

    Дз к уроку №9 работа с файлами

---
+++ src/test/java/guru/qa/Products.java
@@ -0,0 +1,52 @@
+package guru.qa;
+
+public class Products {
+    private String designerName;
+    private String color;
+    private int cost;
+    private int size;
+    private boolean inStock;
+
+    public Products() {
+    }
+
+    public double getCost() {
+        return this.cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getDesignerName() {
+        return designerName;
+    }
+
+    public void setDesignerName(String designerName) {
+        this.designerName = designerName;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
+}
---
+++ src/test/java/guru/qa/ZSNHomeworkFileTest.java
@@ -0,0 +1,69 @@
+package guru.qa;
+
+import com.codeborne.xlstest.XLS;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.opencsv.CSVReader;
+import com.codeborne.pdftest.PDF;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.zip.ZipFile;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ZSNHomeworkFileTest {
+    private final ClassLoader classLoader = ZSNHomeworkFileTest.class.getClassLoader();
+
+    private InputStream getFileFromZip(String fileName) throws Exception {
+        ZipFile zip = new ZipFile(new File("src/test/resources/testFile.zip"));
+        return zip.getInputStream(zip.getEntry(fileName));
+    }
+
+    @Test
+    void pdfTest2() throws Exception {
+        InputStream pdfFileStream = getFileFromZip("61.pdf");
+        PDF pdf = new PDF(pdfFileStream);
+        assertThat(pdf.text).contains("Шляпа лежала на комоде");
+    }
+
+
+    @Test
+    void xlsxTest() throws Exception {
+        XLS xlsx = new XLS(getFileFromZip("testxlsx.xlsx"));
+        assertThat(xlsx.excel.getSheetAt(0).getRow(1).getCell(0).getStringCellValue())
+                .isEqualTo("silk");
+    }
+
+    @Test
+    void csvTest() throws Exception {
+        CSVReader csv = new CSVReader(
+                new InputStreamReader(getFileFromZip("testcsv.csv"), UTF_8)
+        );
+        List<String[]> csvArray = csv.readAll();
+        assertThat(csvArray).contains(
+                new String[]{"silk;black;2"},
+                new String[]{"cashmere;green;4"});
+    }
+
+
+    @Test
+    void jsonTest() throws IOException {
+        var fileName = "testJson.json";
+        try (InputStream inputStream = classLoader.getResourceAsStream(fileName)) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Products product = objectMapper.readValue(inputStream, Products.class);
+            assertThat(product.getDesignerName()).isEqualTo("DIOR");
+            assertThat(product.getColor()).isEqualTo("red");
+            assertThat(product.getSize()).isEqualTo(38);
+            assertThat(product.isInStock()).isEqualTo(true);
+            assertThat(product.getCost()).isEqualTo(129999);
+        }
+    }
+
+}
